06c4caec41ef72d15952a29d7b49616367bcbb5c
Chay Williams <Chayvw@gmail.com>
2021-06-23 17:29:49 -0400

    added mongoose and express npm package

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/losingrecipes", { useNewUrlParser: true },  { useUnifiedTopology: true }).then(()=>{console.log("Connected Successfully")}).catch((err)=>{console.log("Unable to connect")})
+
 app.listen(PORT, () =>{
     console.log(`Express server running on http://localhost:${PORT}`)
 });
+
