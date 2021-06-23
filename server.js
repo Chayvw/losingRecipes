5eb12ddc92341618d1a38d35da21437c98693ac6
Chay Williams <Chayvw@gmail.com>
2021-06-23 18:51:18 -0400

    updated react server to serve react build folder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -7,6 +8,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+} )
 
 app.get("/api/config", (req, res) =>{
     res.json({success:true})
@@ -18,4 +25,23 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/losingrecipes",
 app.listen(PORT, () =>{
     console.log(`Express server running on http://localhost:${PORT}`)
 });
+
+// express app build 
+// touch server.js
+// npm init -y so we can npm i express 
+// require express 
+// app is = instance of express 
+// port is equal process.env to 3001 because React is using 3000
+// app.listen = to port
+
+// add in middle in middleware to access api for our full stack app 
+// npm i mongoose / mongoose.connect tp p.env or local // require mongoose
+
+
+// npx create react app // useEffect to make an axios call to api/config 
+// npm install concurrently 
+// npm install if-env
+
+// static client build servers up a would be built folder statically for heroku
+
 
