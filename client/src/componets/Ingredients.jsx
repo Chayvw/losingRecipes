7de466cf2917a4dc3deebfe5d5449d24cf0df6ba
Chay Williams <Chayvw@gmail.com>
2021-06-24 12:31:14 -0400

    ingredient axios call now renders through jsx onto the the users screen mapping through the array after setState

--- client/src/componets/Ingredients.jsx
+++ client/src/componets/Ingredients.jsx
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Ingredients extends Component {
+    // adding state for my ingredients and setting it to an empty array
+    state = {
+        ingredients: []
+    }
     // componentDidMount because this is a functional component
     componentDidMount() {
         axios.get("api/ingredients")
             .then((response) => {
                 console.log(response.data)
-            })
-            .catch((err) => { console.log(err) })
+                this.setState({
+                    ingredients: response.data.data
+                });
+            }).catch((err) => { console.log(err) });
     }
 
 
@@ -16,7 +22,20 @@ class Ingredients extends Component {
         return (
             <div>
                 <h3>Ingredients</h3>
-            </div>
+                <div>
+                    {this.state.ingredients.map((ingredient) => {
+                        return (
+                            <div key={ingredient._id}>
+                                <h3>{ingredient.name}</h3>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div>
+                    {/* if the length is 0 it will return no ingredients at this time */}
+                    {this.state.ingredients.length === 0 && <p>No ingredients at the moment</p>}
+                </div>
+            </div >
         );
     }
 }
