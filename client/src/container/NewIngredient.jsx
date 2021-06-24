ce44220293f351b37e667862374cc5c40950afaf
Chay Williams <Chayvw@gmail.com>
2021-06-24 17:51:59 -0400

    added handle inputchnage to capture the value and a handlecheckbox to if checked update the state to false

--- client/src/container/NewIngredient.jsx
+++ client/src/container/NewIngredient.jsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NewIngredient(props) {
+    // define state thats going to hold the input value 
+    const [ingredient, setIngredient] = useState("");
+
+    // define state thats going to update the checkbox
+    const [checked, setChecked] = useState(false);
+
+    const handleInputChange= (event) => {
+        // create the event handler that updates the states when the input value changes
+        setIngredient(event.target.value)
+    };
+
+    const handleCheckBoxChange = () =>{
+        setChecked(true);
+
+
+    }
+
+    
+
+    
     return (
         <div className="container">
             <div class="row">
                 <form className="col s12">
                     <div className="row">
                         <div className="input-field col s8">
-                            <input id="name" type="text" />
+                            <input id="name" type="text" value={ingredient} onChange={handleInputChange} />
                             <label htmlFor="email">Ingredient Name</label>
                         </div>
                     </div>
                 </form>
                 <div>
                     <label>
-                        <input className="input-field col s8" type="checkbox" />
+                        <input className="input-field col s8" type="checkbox" onChange={handleCheckBoxChange}/>
                         <span>Vegetarian?</span>
                     </label>
                 </div>
                 <div className="row">
                     <div className="col s4">
-                        <button class="btn waves-effect waves-light" type="submit" name="action">Add
-    <i class="material-icons right">Ingredient</i>
+                        <button className="btn waves-effect waves-light" type="submit" name="action">Add
+    <i className="material-icons right">Ingredient</i>
                         </button>
                     </div>
                 </div>
