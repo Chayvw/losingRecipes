fc891cf8526cb3486a7255f7177b4124e21d3bb0
Chay Williams <Chayvw@gmail.com>
2021-06-24 16:50:36 -0400

    added a forum for user to enter a new ingredient along with a button to submit and selected if vegetarian

--- client/src/container/NewIngredient.jsx
+++ client/src/container/NewIngredient.jsx
@@ -3,8 +3,31 @@ import React from 'react';
 function NewIngredient(props) {
     return (
         <div className="container">
-           <h2>New Ingredient</h2> 
+            <div class="row">
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s8">
+                            <input id="name" type="text" />
+                            <label htmlFor="email">Ingredient Name</label>
+                        </div>
+                    </div>
+                </form>
+                <div>
+                    <label>
+                        <input className="input-field col s8" type="checkbox" />
+                        <span>Vegetarian?</span>
+                    </label>
+                </div>
+                <div className="row">
+                    <div className="col s4">
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Add
+    <i class="material-icons right">Ingredient</i>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
+
     );
 }
 
