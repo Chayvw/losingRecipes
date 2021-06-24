0d9707632b53beeb99f8a4475312e10f921b898a
Chay Williams <Chayvw@gmail.com>
2021-06-24 12:40:37 -0400

    made my NewIngredient.jsx file a class component

---
+++ client/src/container/NewIngredient.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewIngredient extends Component {
+    render() {
+        return (
+            <div>
+               <h1>New Ingredients</h1>  
+            </div>
+        );
+    }
+}
+
+export default NewIngredient;
