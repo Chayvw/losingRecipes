84821e69719b611c2c9cbe20520f9e8cfa370706
Chay Williams <Chayvw@gmail.com>
2021-07-01 08:21:55 -0400

    added user model schema

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+
+    email:{
+        type:String,
+        trim:true,
+        required:"Email address is required."
+    },
+    password:{
+        type: String,
+        trim:true,
+        required:"Email address is required."
+    }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User; 
+
