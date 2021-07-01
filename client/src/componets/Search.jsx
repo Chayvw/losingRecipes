95d47033cf448cbedca23150a23a28236240cd50
Chay Williams <Chayvw@gmail.com>
2021-06-30 20:30:17 -0400

    render axios calls to my browser

--- client/src/componets/Search.jsx
+++ client/src/componets/Search.jsx
@@ -4,52 +4,76 @@ import { Link } from 'react-router-dom';
 // import { response } from 'express';
 
 class Search extends Component {
-    constructor(){
+    constructor() {
         super()
 
-        this.state={
+        this.state = {
             userInput: "",
-            results: ""
+            results: []
         }
 
-        }
-        handleUserInput = (event) =>{
-            event.preventDefault();
-            const query = this.state.userInput;
-            const url = `https://api.edamam.com/search?q=${query}&app_id=c06c8ad2&app_key=02e54c7f11fc157d0fef77be1af9f2ef`
-            axios({
-                url:url,
-                method:'GET'
-            }).then((response)=>{
-                console.log(response)
-                this.setState({
-                    results: response
-
-                })
-                
-            })
-        }
-
-        captureUserInput = (input) => {
+    }
+    handleUserInput = (event) => {
+        event.preventDefault();
+        const query = this.state.userInput;
+        const url = `https://api.edamam.com/search?q=${query}&app_id=c06c8ad2&app_key=02e54c7f11fc157d0fef77be1af9f2ef`
+        axios({
+            url: url,
+            method: 'GET'
+        }).then((response) => {
+            console.log(response.data.hits[0].recipe)
             this.setState({
-                userInput: input
-            }, () => { console.log(input) })
-        
+                results: response.data.hits[0].recipe
+
+            })
+
+        })
+    }
+
+    captureUserInput = (input) => {
+        this.setState({
+            userInput: input
+        }, () => { console.log(input) })
+
 
 
     }
     render() {
         return (
             <div className="container">
-            <div className="row">
-                <div className="col s6">
-                    <Link to='/'> <button className="waves-effect waves-light btn-large">Home</button></Link>
-            
-               <h1>Search Page</h1> 
-               <input type="text"  placeholder="Reciepe Search" value={this.state.userInput} onChange={(event) => { this.captureUserInput(event.target.value) }} />
-               <button onClick={this.handleUserInput}>Search</button>
-            </div>
-            </div>
+                <div className="row">
+                    <div className="col s6">
+                        <Link to='/'> <button className="waves-effect waves-light btn-large">Home</button></Link>
+
+                        <h1>Search Page</h1>
+                        <input type="text" placeholder="Reciepe Search" value={this.state.userInput} onChange={(event) => { this.captureUserInput(event.target.value) }} />
+                        <button onClick={this.handleUserInput}>Search</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s12 m6">
+                        <div class="card blue-grey darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title">{this.state.results.label}</span>
+
+                                {/* <h5>{this.state.results.label}</h5> */}
+          
+                            <p>{this.state.results.source}</p>
+                        
+                            </div>
+                            <div class="card-action">
+                                <a href="#">This is a link</a>
+                                <a href="#">This is a link</a>
+                            </div>
+                        </div>
+                       
+                        {/* {this.state.results.map((result)=>{
+                            <div>
+                                result
+                            </div>
+                        })} */}
+                    </div>
+                </div>
             </div>
         );
     }
